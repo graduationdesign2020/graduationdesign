541bfcd4d2afcf1eb176bd29b1d2deb8deb044bf
xjqnlxx <xjqnlxx@outlook.com>
2020-07-10 08:58:31 +0800

    seeage=feat: web UI

---
+++ web UI-1.0/src/Views/LoginView.js
@@ -0,0 +1,165 @@
+import React, {useContext, useState} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import {history} from "../utils/history";
+import {AuthedContext} from "../utils/authed";
+
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                GraduactionDesign
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '100vh',
+    },
+    image: {
+        backgroundImage: 'url(https://source.unsplash.com/random)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:
+            theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+export default function LoginView() {
+    const {isAuthed, authedSuccess} = useContext(AuthedContext);
+    const classes = useStyles();
+    const [id, setId] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [validid, setValidid] = useState(true);
+    const [validp, setValidp] = useState(true);
+
+    function checkandlog() {
+        if(id == null && password == null){
+            setValidid(false);
+            setValidp(false);
+            return;
+        }
+        if(id == null){
+            setValidid(false);
+            return;
+        }
+        if(password == null){
+            setValidp(false);
+            return;
+        }
+        authedSuccess();
+        history.push('/');
+        return;
+    }
+
+    return (
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        登录账户
+                    </Typography>
+                    <form className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="用户名"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            onChange={event =>{setId(event.target.value);}}
+                            error={!validid}
+                            helperText={!validid?"用户名不可为空":null}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="密码"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={event => {setPassword(event.target.value);}}
+                            error={!validp}
+                            helperText={!validp?"密码不可为空":null}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="记住密码"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={(event)=>{event.preventDefault(); checkandlog();}}
+                        >
+                            登录
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    忘记密码？
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"注册账户"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                        <Box mt={5}>
+                            <Copyright />
+                        </Box>
+                    </form>
+                </div>
+            </Grid>
+        </Grid>
+    );
+}
