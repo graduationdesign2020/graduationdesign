46b8aa5d23b8f6e1a9a6134456df2866a070cebd
Liao Yichen <echo.lyc@sjtu.edu.cn>
2020-07-18 21:17:32 +0800

    add procedure page

--- wechatAppUI/pages/procedure/procedure.js
+++ wechatAppUI/pages/procedure/procedure.js
@@ -1,121 +1,52 @@
 // pages/procedure/procedure.js
+//获取应用实例
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    user: {auth: false, name: "小明"},
-    procedures:[
-      {
-        name:"选题确认",
-        state:2,
-      },
-      {
-        name:"开题报告",
-        state:2,
-      },
-      {
-        name:"第一阶段检查",
-        state:1,
-      },
-      {
-        name:"中期检查",
-        state:0,
-      },
-      {
-        name:"设计(论文)定稿",
-        state:0,
-      }
+    userData: {name: "小明", dept: "SE", auth: 1, id: 12345},
+    states: [
+      {state: "任务书", submit:0, end_time:"07-01 00:00"},
+      {state: "开题报告", submit:1, end_time:"07-10 00:00"},
+      {state: "中期检查", submit:2, end_time:"07-13 00:00"},
+      {state: "论文定稿", submit:3, end_time:"07-18 00:00"},
+      {state: "论文最终稿", submit:4}
     ]
-
   },
 
-  /**
+   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  onChange(e) {
-    console.log(e.detail);
-    switch (e.detail) {
-      case "myprofile": {
-        console.log(e.detail)
-        wx.navigateTo({
-          url: '../myProfile/Center',
-        })
-      }
-      case "QA": {
-        wx.navigateTo({
-          url: '../QA/QA',
-        })
-      }
-      case "studentFinished": {
-        wx.navigateTo({
-          url: '../processList/processList',
-        })
-      }
-      case "procedure": {
-        wx.navigateTo({
-          url: '../procedure/procedure',
-        })
-      }
-      case "home": {
-        wx.navigateTo({
-          url: '../home/home',
-        })
+  onLoad: function() {
+    var that = this;
+    if(app.globalData.login == 2){
+      wx.redirectTo({
+        url: '../register/index',
+      })
+    }else{
+      if(app.globalData.login == 0){
+        app.dataCallback = (data) => {
+          if(data.msg == "FAIL"){
+            wx.redirectTo({
+              url: '../register/index',
+            })
+          }else{
+            this.setData({userData: data.userData})
+            PostRequest('/checkSelfProcess', {stu_id: this.data.userData.id}, that.setStates);
+          }
+        }
+      }else{
+        this.setData({userData: app.globalData.userData});
+        PostRequest('/checkSelfProcess', {stu_id: this.data.userData.id}, that.setStates);
       }
     }
+  },
+
+  setStates: function(data){
+    this.setData({states: data});
   }
 })
