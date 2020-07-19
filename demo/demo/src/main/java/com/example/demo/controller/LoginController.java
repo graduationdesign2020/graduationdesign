7f98570fe10bed640c59aee0edfc7074b9af0c32
riken01 <riken01@sjtu.edu.cn>
2020-07-19 16:39:22 +0800

    feat: get openId

--- demo/demo/src/main/java/com/example/demo/controller/LoginController.java
+++ demo/demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.TeacherMessage;
+
+import com.example.demo.service.HttpClient;
 import com.example.demo.service.LoginService;
+import com.example.demo.utils.CodeReturn;
 import com.example.demo.utils.ReturnInfo;
-import com.example.demo.utils.UserInfo;
+import com.example.demo.constant.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,26 +23,67 @@ public class LoginController {
     LoginService loginService;
 
     @RequestMapping(path = "/register")
-    public ReturnInfo register(@RequestBody Map<String,String> params) {
-        String wechat_id= String.valueOf(params.get("wechat_id"));
+    public ReturnInfo register(@RequestBody Map<String,String> params) throws JsonProcessingException {
+        String code= String.valueOf(params.get("code"));
         String id= String.valueOf(params.get("id"));
         String name=String.valueOf(params.get("name"));
         int teacher= Integer.parseInt(params.get("auth"));
+        String result = "";
+        try {//请求微信服务器，用code换取openid。HttpUtil是工具类，后面会给出实现，Configure类是小程序配置信息，后面会给出代码
+            result = HttpClient.doGet(
+                    "https://api.weixin.qq.com/sns/jscode2session?appid="
+                            +appId.appId+ "&secret="
+                            +appId.secret+ "&js_code="
+                            + code
+                            + "&grant_type=authorization_code", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        CodeReturn openIdJson = mapper.readValue(result, CodeReturn.class);
+        String wechat_id=openIdJson.getOpenid();
         return loginService.register(wechat_id,id,name,teacher);
     }
 
     @RequestMapping(path = "/logout")
-    public ReturnInfo logoff(@RequestBody Map<String,String> params) {
-        String wechat_id=params.get("code");
+    public ReturnInfo logoff(@RequestBody Map<String,String> params) throws JsonProcessingException {
+        String code=params.get("code");
+        String result = "";
+        try {//请求微信服务器，用code换取openid。HttpUtil是工具类，后面会给出实现，Configure类是小程序配置信息，后面会给出代码
+            result = HttpClient.doGet(
+                    "https://api.weixin.qq.com/sns/jscode2session?appid="
+                            +appId.appId+ "&secret="
+                            +appId.secret+ "&js_code="
+                            + code
+                            + "&grant_type=authorization_code", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        CodeReturn openIdJson = mapper.readValue(result, CodeReturn.class);
+        String wechat_id=openIdJson.getOpenid();
         return loginService.logout(wechat_id);
     }
 
     @RequestMapping(path = "/login")
-    public ReturnInfo login(@RequestBody Map<String,String> params){
-        String wechat_id=params.get("code");
-        return loginService.login(wechat_id);
+    public ReturnInfo login(@RequestBody Map<String,String> params) throws JsonProcessingException {
+        String code=params.get("code");
+        String result = "";
+        try {//请求微信服务器，用code换取openid。HttpUtil是工具类，后面会给出实现，Configure类是小程序配置信息，后面会给出代码
+            result = HttpClient.doGet(
+                    "https://api.weixin.qq.com/sns/jscode2session?appid="
+                            +appId.appId+ "&secret="
+                            +appId.secret+ "&js_code="
+                            + code
+                            + "&grant_type=authorization_code", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        CodeReturn openIdJson = mapper.readValue(result, CodeReturn.class);
+        String wechat_id=openIdJson.getOpenid();
+            return loginService.login(wechat_id);
     }
-
 
 }
 
