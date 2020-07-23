99202ab0fd51b30d9872ad0981fd0cd1f2805df4
xjqnlxx <xjqnlxx@outlook.com>
2020-07-23 11:41:20 +0800

    initial crawler

--- bysj/bysj/items.py
+++ bysj/bysj/items.py
@@ -11,7 +11,10 @@ import scrapy
 class StateItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    item_type = scrapy.Field()
     project_id = scrapy.Field()
+    teacher_id = scrapy.Field()
+    teacher_name = scrapy.Field()
     state = scrapy.Field()
     submit = scrapy.Field()
 
@@ -19,6 +22,7 @@ class StateItem(scrapy.Item):
 class StudentItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    item_type = scrapy.Field()
     id = scrapy.Field()
     name = scrapy.Field()
     major = scrapy.Field()
@@ -28,8 +32,19 @@ class StudentItem(scrapy.Item):
 class GradeItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    item_type = scrapy.Field()
     id = scrapy.Field()
     teacher_grade = scrapy.Field()
     review_grade = scrapy.Field()
     defense_grade = scrapy.Field()
     total_grade = scrapy.Field()
+
+class NoticeItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    item_type = scrapy.Field()
+    notice_type = scrapy.Field()
+    title = scrapy.Field()
+    time = scrapy.Field()
+    content = scrapy.Field()
+
