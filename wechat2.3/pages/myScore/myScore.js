5806113ff699a2da3dd2ae53059e457eb2013693
riken01 <riken01@sjtu.edu.cn>
2020-07-29 16:50:49 +0800

    fix: front end problem

--- wechat2.3/pages/myScore/myScore.js
+++ wechat2.3/pages/myScore/myScore.js
@@ -8,32 +8,12 @@ Page({
     reviewgrade: "",
     thesisgrade: "",
     allgrade: "",
-    userData: {},
   },
   
   onLoad: function(){
     var that = this;
-    if(app.globalData.login == 2){
-      wx.redirectTo({
-        url: '../register/index',
-      })
-    }else{
-      if(app.globalData.login == 0){
-        app.dataCallback = (data) => {
-          if(data.msg == "FAIL"){
-            wx.redirectTo({
-              url: '../register/index',
-            })
-          }else{
-            this.setData({userData: data.userData})
-            PostRequest('/getSelfGrade', {id: this.data.userData.id}, that.getGrade);
-          }
-        }
-      }else{
-        this.setData({userData: app.globalData.userData});
-        PostRequest('/getSelfGrade', {id: this.data.userData.id}, that.getGrade);
-      }
-    }
+    this.setData({userData: app.globalData.userData});
+    PostRequest('/getSelfGrade', {}, that.getGrade);
   },
 
   getGrade: function(data) {
--- wechat2.3/pages/noticeDetail/noticeDetail.js
+++ wechat2.3/pages/noticeDetail/noticeDetail.js
@@ -13,7 +13,6 @@ Page({
     notice: {
       title: '标题', id: 1, is_read: false, teacher_id: '12345', student_id: '11111', time: '07-01', content: '内容'
     },
-    userData: {name: "小明", dept: "SE", auth: 1, id: 12345}
   },
 
   /**
@@ -21,57 +20,21 @@ Page({
    */
   onLoad: function(options) {
     var that = this;
-    if(app.globalData.login == 2){
-      wx.redirectTo({
-        url: '../register/index',
-      })
-    }else{
-      if(app.globalData.login == 0){
-        app.dataCallback = (data) => {
-          if(data.msg == "FAIL"){
-            wx.redirectTo({
-              url: '../register/index',
-            })
-          }else{
-            this.setData({userData: data.userData, type: options.type})
-            switch (options.type) {
-              case "0": {
-                PostRequest('/getSchoolNotice', {id: options.id}, that.setNotice);
-                break;
-              }
-              case "1": {
-                PostRequest('/getDepartmentNotice', {id: options.id}, that.setNotice);
-                break;
-              }
-              case "2": {
-                var pages = getCurrentPages();
-                var prevPage = pages[pages.length - 2]; //上一个页面
-                prevPage.setData({isRefresh: true}) 
-                PostRequest('/getTeacherMessage', {id: options.id, reading_id: options.reading_id}, that.setNotice);
-                break;
-              }
-            }
-          }
-        }
-      }else{
-        this.setData({userData: app.globalData.userData, type: options.type});
-        switch (options.type) {
-          case "0": {
-            PostRequest('/getSchoolNotice', {id: options.id}, that.setNotice);
-            break;
-          }
-          case "1": {
-            PostRequest('/getDepartmentNotice', {id: options.id}, that.setNotice);
-            break;
-          }
-          case "2": {
-            var pages = getCurrentPages();
-            var prevPage = pages[pages.length - 2]; //上一个页面
-            prevPage.setData({isRefresh: true}) 
-            PostRequest('/getTeacherMessage', {id: options.id, reading_id: options.reading_id}, that.setNotice);
-            break;
-          }
-        }
+    switch (options.type) {
+      case "0": {
+        PostRequest('/getSchoolNotice', {id: options.id}, that.setNotice);
+        break;
+      }
+      case "1": {
+        PostRequest('/getDepartmentNotice', {id: options.id}, that.setNotice);
+        break;
+      }
+      case "2": {
+        var pages = getCurrentPages();
+        var prevPage = pages[pages.length - 2]; //上一个页面
+        prevPage.setData({isRefresh: true}) 
+        PostRequest('/getTeacherMessage', {id: options.id, reading_id: options.reading_id}, that.setNotice);
+        break;
       }
     }
   },
