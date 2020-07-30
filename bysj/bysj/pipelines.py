8815c4165440077b66207c018f71923520074897
Liao Yichen <echo.lyc@sjtu.edu.cn>
2020-07-30 15:10:42 +0800

    feat: test crawl

--- bysj/bysj/pipelines.py
+++ bysj/bysj/pipelines.py
@@ -136,6 +136,7 @@ class BYSJPipeline(object):
 
         if item["item_type"] == 'state':
             sql = "INSERT INTO teacher (id, name, department, major) SELECT %s, %s, %s, %s FROM DUAL WHERE NOT EXISTS ( SELECT * FROM teacher WHERE id = %s)"
+            insert_user = """insert into wechatusers(wechat_id, id, auth) SELECT %s, %s, %s FROM DUAL WHERE NOT EXISTS ( SELECT * FROM wechatusers WHERE wechat_id = %s)"""
             try:
                 # 执行sql语句
                 self.cursor.execute(sql, (
@@ -162,6 +163,30 @@ class BYSJPipeline(object):
                     item['teacher_id'] + 'k', item['teacher_name'], "电子信息与电气工程学院", "电气信息", item['teacher_id'] + 'k'))
                 self.cursor.execute(sql, (
                     item['teacher_id'] + 'l', item['teacher_name'], "电子信息与电气工程学院", "人工智能", item['teacher_id'] + 'l'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'a', item['teacher_id'] + 'a', 'ROLE_TEACHER', item['teacher_id'] + 'a'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'b', item['teacher_id'] + 'b', 'ROLE_TEACHER', item['teacher_id'] + 'b'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'c', item['teacher_id'] + 'c', 'ROLE_TEACHER', item['teacher_id'] + 'c'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'd', item['teacher_id'] + 'd', 'ROLE_TEACHER', item['teacher_id'] + 'd'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'e', item['teacher_id'] + 'e', 'ROLE_TEACHER', item['teacher_id'] + 'e'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'f', item['teacher_id'] + 'f', 'ROLE_TEACHER', item['teacher_id'] + 'f'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'g', item['teacher_id'] + 'g', 'ROLE_TEACHER', item['teacher_id'] + 'g'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'h', item['teacher_id'] + 'h', 'ROLE_TEACHER', item['teacher_id'] + 'h'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'i', item['teacher_id'] + 'i', 'ROLE_TEACHER', item['teacher_id'] + 'i'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'j', item['teacher_id'] + 'j', 'ROLE_TEACHER', item['teacher_id'] + 'j'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'k', item['teacher_id'] + 'k', 'ROLE_TEACHER', item['teacher_id'] + 'k'))
+                self.cursor.execute(insert_user, (
+                    item['teacher_id'] + 'l', item['teacher_id'] + 'l', 'ROLE_TEACHER', item['teacher_id'] + 'l'))
                 self.connect.commit()
             except:
                 self.connect.rollback()
