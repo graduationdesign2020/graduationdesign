331d5713e35ff6efae15a1b79a865e9228a14ba7
riken01 <riken01@sjtu.edu.cn>
2020-08-21 19:30:55 +0800

    fix: rename

--- demo/gdms-search/src/main/java/org/graduationdesign/search/SearchApplication.java
+++ demo/gdms-search/src/main/java/org/graduationdesign/search/SearchApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package org.graduationdesign.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- demo/gdms-search/src/main/java/org/graduationdesign/search/controller/MessageController.java
+++ demo/gdms-search/src/main/java/org/graduationdesign/search/controller/MessageController.java
@@ -1,7 +1,7 @@
-package com.example.demo.controller;
+package org.graduationdesign.search.controller;
 
-import com.example.demo.entity.Message;
-import com.example.demo.service.MessageService;
+import org.graduationdesign.search.entity.Message;
+import org.graduationdesign.search.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
