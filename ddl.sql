72136061cbf487f357c9fd249701ad9a1673acae
riken01 <riken01@sjtu.edu.cn>
2020-09-06 23:16:01 +0800

    mfeat: new table

--- ddl.sql
+++ ddl.sql
@@ -5,7 +5,6 @@ DROP TABLE IF EXISTS schoolnotice;
 DROP TABLE IF EXISTS deptnotice;
 DROP TABLE IF EXISTS teachermessage;
 DROP TABLE IF EXISTS teachermessagereading;
-DROP TABLE IF EXISTS teachermessagereply;
 DROP TABLE IF EXISTS wechatusers;
 drop table if exists principleusers;
 DROP TABLE IF EXISTS project;
@@ -138,23 +137,13 @@ create table teachermessagereading
 	foreign key (message_id) references teachermessage (id) on delete cascade
 );
 
-create table teachermessagereply
-(
-    id         int auto_increment,
-    message_id int                  not null,
-    student_id varchar(25)          not null,
-    is_reply    boolean default 0 not null,
-    primary key(id),
-	foreign key (student_id) references student (id) on delete cascade,
-	foreign key (message_id) references teachermessage (id) on delete cascade
-);
-    
 create table schedulejob(
 	id				int NOT NULL auto_increment,
 	teacher_id 		varchar(30) NOT NULL,  
     job_status		varchar(2) NOT NULL default"0",  
     state			int not null,  
-    end_time		timestamp not null,
+    end_date		date not null,
+    end_time		int not null,
     PRIMARY KEY (id),
     FOREIGN KEY (teacher_id) REFERENCES teacher(id) on delete cascade
 );
